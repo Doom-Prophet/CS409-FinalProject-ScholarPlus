91a02ab069a121a0ddee55b4769eb35eed104d10
Zicheng Ma <zicheng5@illinois.edu>
2022-12-06 03:35:13 -0600

    start up, more following

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@
  */
 module.exports = function (app, router) {
     app.use('/api', require('./home.js')(router));
+    app.get("/api",(req,res)=>{
+        const name = req.query.user;
+        res.send(name+"!");
+    })
 };
